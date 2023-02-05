b50ee0f0ef31fd1b30bb27d1bf5fe41de0cdf3d4
Barto Molina <bartomolina@gmail.com>
2023-02-05 01:40:32 +0100

    Integrating p5

--- pages/collections/[id].tsx
+++ pages/collections/[id].tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import Image from "next/image";
 import { remark } from "remark";
 import html from "remark-html";
@@ -17,13 +18,11 @@ const CollectionItem = () => {
     if (projectId) {
       execute(ArtblocksCollectionDocument, { id: projectId })
         .then((result) => {
-          console.log(result);
           setCollection(result?.data.project);
           setSrc(`https://media.artblocks.io/thumb/${result?.data.project.projectId}000000.png`);
           return remark().use(html).process(result?.data.project.description);
         })
         .then((result) => {
-          console.log(result.toString());
           setDescription(result.toString());
         });
     }
@@ -48,20 +47,44 @@ const CollectionItem = () => {
                   onError={() => setSrc("/preview-error.png")}
                 />
               )}
-              <div className="mx-0.5 mt-3">
-                <div className="font-semibold text-2xl text-gray-900">
-                  {collection?.name} <span className="ml-2">#{collection?.projectId}</span>
-                </div>
-                <div className="-mt-0.5 text-xl text-gray-700">{collection?.artistName}</div>
-                <div className="mt-4 text-gray-500">
-                  <div>
-                    {collection?.invocations}
-                    {collection?.invocations != collection?.maxInvocations && ` / ${collection?.maxInvocations}`}
-                    {" minted"}
+              <div className="mx-0.5 mt-3 divide-y-2">
+                <div className="mb-4">
+                  <div className="flex justify-between items-baseline space-x-2 font-semibold text-2xl text-gray-900 mr-4">
+                    <div>{collection?.name}</div>
+                    <div>#{collection?.projectId}</div>
                   </div>
-                  {collection?.minterConfiguration?.startTime && (
-                    <div>{new Date(collection?.minterConfiguration.startTime * 1000).toUTCString()}</div>
-                  )}
+                  <div className="-mt-0.5 text-xl text-gray-700">{collection?.artistName}</div>
+                  <a
+                    href={collection?.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline text-gray-500"
+                  >
+                    {collection?.website}
+                  </a>
+                </div>
+                <div className="pt-4">
+                  <div className="text-gray-500">
+                    <div>
+                      {collection?.invocations}
+                      {collection?.invocations != collection?.maxInvocations && ` / ${collection?.maxInvocations}`}
+                      {" minted"}
+                    </div>
+                    {collection?.minterConfiguration?.startTime && (
+                      <div>{new Date(collection?.minterConfiguration.startTime * 1000).toUTCString()}</div>
+                    )}
+                  </div>
+                  <div className="mt-3 text-gray-500">
+                    <div>{collection?.scriptTypeAndVersion}</div>
+                    <div>{collection?.license}</div>
+                  </div>
+                  <div className="mt-4 flex justify-center">
+                    <Link
+                      href={`/editor/${collection?.id}`}
+                      className="rounded-lg bg-indigo-600 px-3 py-2 text-white shadow-md hover:bg-indigo-500 active:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:ring-offset-2">
+                      Open in editor
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
--- pages/editor/[id].tsx
+++ pages/editor/[id].tsx
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import Image from "next/image";
-import Editor from "react-simple-code-editor";
+import dynamic from "next/dynamic";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 import { ArtblocksCollectionDocument, ArtblocksCollectionQuery, execute } from "../../.graphclient";
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism.css";
 import prettier from "prettier/esm/standalone.mjs";
 import parserBabel from "prettier/esm/parser-babel.mjs";
 
@@ -15,15 +12,28 @@ const CollectionItem = () => {
   const router = useRouter();
   const projectId = router.query.id as string;
   const [collection, setCollection] = useState<ArtblocksCollectionQuery>();
-  const [src, setSrc] = useState("/preview-error.png");
-  const [code, setCode] = useState(`function add(a, b){return a + b;}`);
+  const [code, setCode] = useState("");
+
+  const scri = "alert('hello');";
+
+  // useEffect(() => {
+  //   const script = document.createElement('script');
+
+  //   script.src = "alert('hello');";
+  //   script.async = true;
+  
+  //   document.body.appendChild(script);
+  
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   }
+  // }, []);
 
   useEffect(() => {
     if (projectId) {
       execute(ArtblocksCollectionDocument, { id: projectId }).then((result) => {
         setCollection(result?.data.project);
         console.log(result);
-        setSrc(`https://media.artblocks.io/thumb/${result?.data.project.projectId}000000.png`);
 
         const formattedCode = prettier.format(result?.data.project.script, {
           parser: "babel",
@@ -42,42 +52,25 @@ const CollectionItem = () => {
         <meta name="description" content="CodArt" />
       </Head>
       <div className="bg-gray-100 pb-24">
-        <div className="mx-auto max-w-6xl sm:px-6 lg:px-8 py-6">
-          <div className="grid grid-cols-4">
-            <div>
-              {collection && (
-                <Image
-                  width={255}
-                  height={255}
-                  src={src}
-                  alt={collection?.name as string}
-                  onError={() => setSrc("/preview-error.png")}
-                />
-              )}
-              <div className="mx-0.5 mt-3">
-                <div className="font-semibold text-lg text-gray-900">{collection?.name}</div>
-                <div className="-mt-1 text-sm text-gray-500">{collection?.artistName}</div>
-                <div className="-mt-1 text-sm text-gray-500">{collection?.artistName}</div>
-              </div>
+        <div className="mx-auto sm:px-6 lg:px-8 py-6">
+          <div className="grid grid-cols-2 space-x-8">
+            <div className=" bg-white rounded">
+              <CodeMirror
+                value={code}
+                height="800px"
+                extensions={[javascript()]}
+                // onChange={onChange}
+              />
             </div>
-
-            {collection && (
-              <div className="col-span-3 bg-white rounded">
-                <Editor
-                  value={code}
-                  onValueChange={(code) => setCode(code)}
-                  highlight={(code) => highlight(code, languages.js)}
-                  padding={10}
-                  style={{
-                    fontFamily: '"Fira code", "Fira Mono", monospace',
-                    fontSize: 12,
-                  }}
-                />
-              </div>
-            )}
+            <div></div>
           </div>
         </div>
       </div>
+      <script
+            dangerouslySetInnerHTML={{
+              __html: scri,
+            }}
+          ></script>
     </>
   );
 };
