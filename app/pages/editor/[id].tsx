04fab10e1931bff9ac670a4fe1140fc0eb7936b0
Barto Molina <bartomolina@gmail.com>
2023-02-08 13:10:10 +0100

    Added tokenHash

--- app/pages/editor/[id].tsx
+++ app/pages/editor/[id].tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
+import { FormEvent, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { minify } from "terser";
@@ -13,10 +13,12 @@ const CollectionItem = () => {
   const projectId = router.query.id as string;
   const [collection, setCollection] = useState<ArtblocksCollectionQuery>();
   const [code, setCode] = useState("");
+  const [tokenHash, setTokenHash] = useState("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
 
+  const tokenData = useMemo(() => {
+    return `window.tokenData={hash: "${tokenHash}"};`;
+  }, [tokenHash]);
   const p5script = `<script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js"></script>`;
-
-  const tokenData = `window.tokenData={hash: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"};`;
 
   const handleRun = (event: FormEvent) => {
     event.preventDefault();
@@ -61,9 +63,21 @@ const CollectionItem = () => {
         <meta name="description" content="CodArt" />
       </Head>
       <div className="bg-gray-100 pb-24">
-        <div className="mx-auto sm:px-6 lg:px-8 py-6">
+        <div className="mx-auto max-w-6xl sm:px-6 lg:px-8 pb-44 pt-5">
           <div>
             <div className="flex">
+              <div>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    id="hash"
+                    name="hash"
+                    value={tokenHash}
+                    onChange={(event) => setTokenHash(event.target.value)}
+                    className="mr-3 block max-w-fit rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
               <button
                 type="button"
                 onClick={handleRun}
@@ -78,21 +92,14 @@ const CollectionItem = () => {
               >
                 Create collection
               </button>
-              {/* <Link
-                href={{ pathname: "/create", query: { script: code } }}
-                as="/create"
-                className="ml-3 text-center rounded-lg bg-indigo-600 px-3 py-2 text-white shadow-md hover:bg-indigo-500 active:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:ring-offset-2"
-              >
-                Create collection
-              </Link> */}
             </div>
           </div>
-          <div className="grid grid-cols-2 space-x-8 mt-4">
-            <div className=" bg-white rounded">
+          <div className="grid grid-cols-8 space-x-8 mt-4">
+            <div className="bg-white rounded col-span-5">
               <CodeMirror value={code} height="800px" extensions={[javascript()]} onChange={(code) => setCode(code)} />
             </div>
             <main>
-              <iframe width={600} height={800} id="canvasIframe" />
+              <iframe width={600} height={800} scrolling="no" className="overflow-hidden" id="canvasIframe" />
             </main>
           </div>
         </div>
