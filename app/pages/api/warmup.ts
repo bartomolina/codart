8d68b57e22c9b73bf80273af7ff7fa7da65bcc7f
Barto Molina <bartomolina@gmail.com>
2023-02-22 12:55:46 +0100

    Performance tests

--- app/pages/api/warmup.ts
+++ app/pages/api/warmup.ts
@@ -1,24 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-const warmUpSeconds = 10 * 60 * 60;
+// const warmUpSeconds = 10 * 60 * 60;
+const warmUpSeconds = 1;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const dev = process.env.NODE_ENV !== 'production';
-  const protocol = dev ? 'http://' : 'https://';
+  let log = "warming up";
+  const dev = process.env.NODE_ENV !== "production";
+  const protocol = dev ? "http://" : "https://";
 
   global.lastWarmUp = global.lastWarmUp || 0;
   const secondsSinceLastWarmUp = (new Date().getTime() - global.lastWarmUp) / 1000;
 
+  log += "/seconds sice last warmup: " + secondsSinceLastWarmUp;
+
   if (secondsSinceLastWarmUp > warmUpSeconds) {
-    console.log("----------------- PERFORMANCE WARM UP START -----------------");
+    console.log("----------------- WARM UP START -----------------");
+    log += "/warming up";
     global.lastWarmUp = new Date().getTime();
     console.time("warmup");
-    return fetch(`${protocol}${req.headers.host}/collections/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270-370`).then(() => {
-      console.timeEnd("warmup");
-      console.log("----------------- PERFORMANCE WARM UP END -----------------");
-      return res.status(200).end();
-    });
+    return fetch(`${protocol}${req.headers.host}/collections/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270-370`).then(
+      () => {
+        console.timeEnd("warmup");
+        log += "/warmup successful";
+        console.log("----------------- WARM UP END -----------------");
+        return res.status(200).json({ result: log });
+      }
+    );
   }
 
-  return res.status(200).end();
+  return res.status(200).json({ result: log });
 }
