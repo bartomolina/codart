77247bd2b7d5b805541030e63781f9fcf713ba26
Barto Molina <bartomolina@gmail.com>
2023-02-22 11:09:14 +0100

    Performance tests

--- app/pages/collections/[id].tsx
+++ app/pages/collections/[id].tsx
@@ -2,11 +2,8 @@ import { IABCollection } from "../../global";
 import { useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
 import { remark } from "remark";
 import html from "remark-html";
-import { getCollectionDataFromFS } from "../../lib/artblocks-cache";
 
 type Props = {
   collection: IABCollection;
@@ -40,56 +37,7 @@ const CollectionItem = ({ collection }: Props) => {
       <div className="bg-gray-100 pb-14">
         <div className="mx-auto max-w-6xl sm:px-6 lg:px-8 py-6">
           <div className="grid grid-cols-3 space-x-4">
-            <div>
-              {collection && src && (
-                <Image
-                  width={350}
-                  height={350}
-                  src={src}
-                  alt={collection?.name as string}
-                  onError={() => setSrc("/preview-error.png")}
-                />
-              )}
-              <div className="mx-0.5 mt-3 divide-y-2">
-                <div className="mb-4">
-                  <div className="flex justify-between items-baseline space-x-2 font-semibold text-2xl text-gray-900 mr-4">
-                    <div>by {collection?.artistName}</div>
-                    <div>#{collection?.projectId}</div>
-                  </div>
-                  <a
-                    href={collection?.website}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="hover:underline text-gray-500"
-                  >
-                    {collection?.website}
-                  </a>
-                </div>
-                <div className="pt-4">
-                  <div className="text-gray-500">
-                    <div>
-                      {collection?.invocations}
-                      {collection?.invocations != collection?.maxInvocations && ` / ${collection?.maxInvocations}`}
-                      {" minted"}
-                    </div>
-                    {date && <div>{new Date(date * 1000).toUTCString()}</div>}
-                  </div>
-                  <div className="mt-3 text-gray-500">
-                    <div>{collection?.scriptType}</div>
-                    <div>{collection?.license}</div>
-                  </div>
-                  <div className="mt-4 flex justify-center">
-                    <Link
-                      href={`/create/${collection?.id}`}
-                      className="w-full text-center rounded-lg bg-indigo-600 px-3 py-2 text-white shadow-md hover:bg-indigo-500 active:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:ring-offset-2"
-                    >
-                      Open in editor
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-span-2 markdown text-lg" dangerouslySetInnerHTML={{ __html: description }} />
+            test
           </div>
         </div>
       </div>
